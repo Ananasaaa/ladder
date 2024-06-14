52e8b3d91dec8afe2b712a251dc81862b4a2960f
Ananasaaa <kairyakn@gmail.com>
2024-06-14 14:55:05 +0300

    lesson_8.1

---
+++ script.js
@@ -0,0 +1,32 @@
+let ladder = {
+
+i : 0,
+
+up:function() {
+   this.i++;
+   return this;
+},
+
+down:function() {
+   this.i--;
+   return this;
+},
+
+showStep:function() {
+    console.log(this.i);
+    return this.i;
+}
+
+}
+
+
+
+
+ladder.up().up().down().up().up().up().up().up().up().showStep();
+
+
+
+
+
+
+  

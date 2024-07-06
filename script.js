7c3b72b83d81a9c938fd581ae9dba8dbb3660ee4
Ananasaaa <kairyakn@gmail.com>
2024-07-06 18:33:47 +0300

    modified

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-let ladder = {
-
-i : 0,
-
-up:function() {
-   this.i++;
-   return this;
-},
-
-down:function() {
-   this.i--;
-   return this;
-},
-
-showStep:function() {
-    console.log(this.i);
-    return this.i;
-}
-
-}
-
-
-
+const ladder = {
+   i: 0,
+   up: function() {
+     this.i++;
+     return this;
+   },
+   down: function() {
+      this.i--;
+      return this;
+   },
+   showStep: function() {
+      console.log(this.i);
+      return this.i;
+   }
+};
 
 ladder.up().up().down().up().up().up().up().up().up().showStep();
-
-
-
-
-
-
-  
